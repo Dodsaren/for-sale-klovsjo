4f038f85221a281796b4a71b26e6ed6aee7daa10
Jim Nilsson <jim.nilsson@sbab.se>
2020-08-29 19:13:57 +0200

    foo

--- docs/public.a5b1e6c6.js
+++
@@ -1,2 +0,0 @@
-window.onload=function(){document.getElementById("open").addEventListener("click",(function(){document.getElementById("lightbox").classList.remove("dn"),document.getElementById("body").classList.add("disable-body-scroll")})),document.getElementById("close").addEventListener("click",(function(){document.getElementById("lightbox").classList.add("dn"),document.getElementById("body").classList.remove("disable-body-scroll")}))};
-//# sourceMappingURL=public.a5b1e6c6.js.map
