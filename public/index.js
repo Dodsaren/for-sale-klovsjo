0b215f6d35516b7c323d97aacdd6e5574b2e6714
Jim Nilsson <jimnils@hotmail.com>
2020-08-30 11:02:40 +0200

    count on close

--- public/index.js
+++ public/index.js
@@ -2,16 +2,15 @@ window.onload = function () {
   var intervalHandler
   var counter = 0
   document.getElementById('open').addEventListener('click', function () {
-    document.getElementById('lightbox').classList.remove('dn')
-    document.getElementById('body').classList.add('disable-body-scroll')
     window.gtag('event', 'Open', {
       event_category: 'Lightbox image gallery',
     })
+    document.getElementById('lightbox').classList.remove('dn')
+    document.getElementById('body').classList.add('disable-body-scroll')
     intervalHandler = setInterval(function () {
       counter += 1
       window.gtag('event', 'Browsing', {
         event_category: 'Lightbox image gallery',
-        value: counter,
       })
     }, 1000)
   })
@@ -20,6 +19,7 @@ window.onload = function () {
     document.getElementById('body').classList.remove('disable-body-scroll')
     window.gtag('event', 'Close', {
       event_category: 'Lightbox image gallery',
+      value: counter,
     })
     clearInterval(intervalHandler)
     counter = 0
