8e44d5e6294d2d020f67da892d3d34743211a61b
Jim Nilsson <jimnils@hotmail.com>
2020-09-01 23:32:25 +0200

    hello

--- public/index.js
+++ public/index.js
@@ -24,4 +24,30 @@ window.onload = function () {
     clearInterval(intervalHandler)
     counter = 0
   })
+
+  var intervalHandler2
+  var counter2 = 0
+  document.getElementById('open2').addEventListener('click', function () {
+    window.gtag('event', 'Open', {
+      event_category: 'Lightbox blueprint gallery',
+    })
+    document.getElementById('lightbox2').classList.remove('dn')
+    document.getElementById('body').classList.add('disable-body-scroll')
+    intervalHandler2 = setInterval(function () {
+      counter2 += 1
+      window.gtag('event', 'Browsing', {
+        event_category: 'Lightbox blueprint gallery',
+      })
+    }, 1000)
+  })
+  document.getElementById('close2').addEventListener('click', function () {
+    document.getElementById('lightbox2').classList.add('dn')
+    document.getElementById('body').classList.remove('disable-body-scroll')
+    window.gtag('event', 'Close', {
+      event_category: 'Lightbox blueprint gallery',
+      value: counter2,
+    })
+    clearInterval(intervalHandler2)
+    counter2 = 0
+  })
 }
